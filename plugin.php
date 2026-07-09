f19109441bb035ce25f8ec1b09d0013bb590fd08
Dylan Duff <dylanduff00@gmail.com>
2026-07-09 22:29:18 +1000

    Release v1.4.6

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WP Toolkit
  * Description: Modular WP admin tweaks loaded from individual drop-in files.
- * Version: 1.4.5
+ * Version: 1.4.6
  * Author:      Dylan Duff
  * Author URI:  https://dylanduff.com
  * GitHub URI:  https://github.com/DylanDuff/wp-toolkit
